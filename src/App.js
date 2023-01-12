733a98f01c8af344a0e24153d4bab0aa86ceb50e
arjav310 <arjav.patel97@gmail.com>
2023-01-12 11:37:40 -0600

    setted up tailwind css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1 className="w-screen h-screen flex justify-center text-center">
+        Welcome
+      </h1>
     </div>
   );
 }
