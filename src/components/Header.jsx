8c2715a02940db89b5e50d82925634ffbbd04760
arjav310 <arjav.patel97@gmail.com>
2023-01-15 11:54:41 -0600

    hamburger menu done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ import "./header.css";
 
 const Header = () => {
   const globalContext = useContext(GlobalContext);
-  const [isMenu, setIsMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const firebaseAuth = getAuth(app);
   const authProvider = new GoogleAuthProvider();
@@ -23,14 +23,16 @@ const Header = () => {
         localStorage.setItem("user", JSON.stringify(res.user.providerData[0]));
       });
     } else {
-      setIsMenu(!isMenu);
+      setShowMenu(!showMenu);
     }
   };
   let userData = JSON.parse(localStorage.getItem("user"));
   const handleLogout = () => {
     localStorage.clear();
-    setIsMenu(!isMenu);
+    setShowMenu(!showMenu);
   };
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
     <header className="fixed z-50 w-screen p-6 px-8 md:p-6 md:px-16">
@@ -77,14 +79,20 @@ const Header = () => {
                   }
                   onClick={login}
                 />
-                {isMenu && (
+                {showMenu && (
                   <motion.div
                     initial={{ opacity: 0, scale: 0.1 }}
                     animate={{ opacity: 1, scale: 1 }}
                     exit={{ opacity: 0, scale: 0.2 }}
                     className="w-40 bg-gray-50 shadow-lg flex flex-col absolute top-11 right-0"
                   >
-                    <Link className="p-3 rounded-t-lg flex items-center justify-center gap-3 cursor-pointer bg-gray-100  hover:bg-cartNumBg hover:text-white transition-all duration-100 ease-in-out text-textColor text-base">
+                    <Link
+                      to={"/createItem"}
+                      className="p-3 rounded-t-lg flex items-center justify-center gap-3 cursor-pointer bg-gray-100  hover:bg-cartNumBg hover:text-white transition-all duration-100 ease-in-out text-textColor text-base"
+                      onClick={() => {
+                        setShowMenu(!showMenu);
+                      }}
+                    >
                       New Item <MdAdd className="text-2xl" />
                     </Link>
                     <Link
@@ -102,6 +110,53 @@ const Header = () => {
       </div>
       {/* mobile */}
       <div className="flex md:hidden w-full h-full item-center justify-between">
+        <section className="flex items-center md:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="block h-0.5 w-7 bg-gray-600"></span>
+            <span className="block h-0.5 w-7 bg-gray-600"></span>
+            <span className="block h-0.5 w-7 bg-gray-600"></span>
+          </div>
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+              <Link
+                to={"/"}
+                onClick={() => setIsNavOpen((prev) => !prev)}
+                className="text-base text-textColor hover:text-cartNumBg duration-100 transition-all ease-in-out cursor-pointer"
+              >
+                Home
+              </Link>
+              <li className="text-base text-textColor hover:text-cartNumBg duration-100 transition-all ease-in-out cursor-pointer">
+                Menu
+              </li>
+              <li className="text-base text-textColor hover:text-cartNumBg duration-100 transition-all ease-in-out cursor-pointer">
+                About Us
+              </li>
+              <li className="text-base text-textColor hover:text-cartNumBg duration-100 transition-all ease-in-out cursor-pointer">
+                Service
+              </li>
+            </ul>
+          </div>
+        </section>
         <Link to={"/"} className="flex items-center gap-2 cursor-pointer">
           <img className="w-8 object-cover" src={Logo} alt="logo" />
           <h3 className="text-headingColor text-xl font-bold">City</h3>
@@ -120,14 +175,20 @@ const Header = () => {
             }
             onClick={login}
           />
-          {isMenu && (
+          {showMenu && (
             <motion.div
               initial={{ opacity: 0, scale: 0.1 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.2 }}
               className="w-40 bg-gray-50 shadow-lg flex flex-col absolute top-11 right-0"
             >
-              <Link className="p-3 rounded-t-lg flex items-center justify-center gap-3 cursor-pointer bg-gray-100  hover:bg-cartNumBg hover:text-white transition-all duration-100 ease-in-out text-textColor text-base">
+              <Link
+                to={"/createItem"}
+                className="p-3 rounded-t-lg flex items-center justify-center gap-3 cursor-pointer bg-gray-100  hover:bg-cartNumBg hover:text-white transition-all duration-100 ease-in-out text-textColor text-base"
+                onClick={() => {
+                  setShowMenu(!showMenu);
+                }}
+              >
                 New Item <MdAdd className="text-2xl" />
               </Link>
               <Link
